7cb9fade4e179f6d248146c3bcb33e21e40fa59d
KenTandrian <kenricktan11@gmail.com>
2023-01-18 13:45:52 +0700

    Change back similar to original vendor file

--- app/Http/Middleware/ValidateHttpsSignature.php
+++ app/Http/Middleware/ValidateHttpsSignature.php
@@ -49,7 +49,7 @@ class ValidateHttpsSignature
 
     /**
      * Copied and modified from
-     * vendor/laravel/framework/src/Illuminate/Routing/UrlGenerator.php:363.
+     * vendor/laravel/framework/src/Illuminate/Routing/UrlGenerator.php:394.
      *
      * Determine if the given request has a valid signature.
      *
@@ -59,11 +59,15 @@ class ValidateHttpsSignature
      */
     public function hasValidSignature(Request $request, $absolute = true)
     {
+        // $ignoreQuery removed on the next line
         return $this->hasCorrectSignature($request, $absolute)
             && $this->signatureHasNotExpired($request);
     }
 
     /**
+     * Copied and modified from
+     * vendor/laravel/framework/src/Illuminate/Routing/UrlGenerator.php:420.
+     *
      * Determine if the signature from the given request matches the URL.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -75,12 +79,13 @@ class ValidateHttpsSignature
     {
         $url = $absolute ? $request->url() : '/'.$request->path();
 
-        // THE FIX:
+        // The Fix - Start
         $url = str_replace('http://', 'https://', $url);
 
         $original = rtrim($url.'?'.Arr::query(
             Arr::except($request->query(), 'signature')
         ), '?');
+        // The Fix - End
 
         $signature = hash_hmac('sha256', $original, call_user_func($this->keyResolver));
 
@@ -89,6 +94,9 @@ class ValidateHttpsSignature
     }
 
     /**
+     * Copied and modified from
+     * vendor/laravel/framework/src/Illuminate/Routing/UrlGenerator.php:443.
+     *
      * Determine if the expires timestamp from the given request is not from the past.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -96,9 +104,9 @@ class ValidateHttpsSignature
      */
     public function signatureHasNotExpired(Request $request)
     {
-        /** @psalm-suppress PossiblyInvalidCast */
-        $expires = (int) $request->query('expires');
+        $expires = $request->query('expires');
 
+        /** @phpstan-ignore-next-line */
         return ! ($expires && Carbon::now()->getTimestamp() > $expires);
     }
 }
